d809485e757722c414719123cd8f5083862e0d84
Edric D <datdt@ftech.ai>
2021-07-15 13:35:51 +0700

    removed dependency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftPackage",
-            dependencies: ["SPMDeveloperInsider", "AppAuth-iOS"]),
+            dependencies: ["SPMDeveloperInsider", "AppAuth"]),
         .testTarget(
             name: "SwiftPackageTests",
             dependencies: ["SwiftPackage"]),
