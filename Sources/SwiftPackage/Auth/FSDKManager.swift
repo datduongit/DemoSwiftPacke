b9bdd2df1f1d124ceb4e0d7ea5994e7d45a3712b
Edric D <datdt@ftech.ai>
2021-07-15 13:39:17 +0700

    removed dependency

--- Sources/SwiftPackage/Auth/FSDKManager.swift
+++ Sources/SwiftPackage/Auth/FSDKManager.swift
@@ -4,19 +4,19 @@
 //
 //  Created by Edric D. on 15/07/2021.
 //
-import AppAuth
-//import SPMDeveloperInsider
+//import AppAuth
+import SPMDeveloperInsider
 
 public class FSDKManager {
     public static func login() {
         print("login")
-//        let sum = SPMDeveloperInsider.init().Sum(num1: 1, num2: 4)
-//        print(sum)
+        let sum = SPMDeveloperInsider.init().Sum(num1: 1, num2: 4)
+        print(sum)
 
 
-        OIDAuthorizationService.discoverConfiguration(forIssuer: URL(string: "")!) { _, _ in
-
-        }
+//        OIDAuthorizationService.discoverConfiguration(forIssuer: URL(string: "")!) { _, _ in
+//
+//        }
     }
 }
 
