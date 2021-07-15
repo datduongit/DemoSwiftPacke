2dffbf17284b088496605af37d2cd35df8b7f6f8
Edric D <datdt@ftech.ai>
2021-07-15 13:13:20 +0700

    add library

--- Sources/SwiftPackage/Auth/FSDKManager.swift
+++ Sources/SwiftPackage/Auth/FSDKManager.swift
@@ -4,10 +4,14 @@
 //
 //  Created by Edric D. on 15/07/2021.
 //
+import AppAuth
+import SPMDeveloperInsider
 
 public class FSDKManager {
     public static func login() {
         print("login")
+        let sum = SPMDeveloperInsider.init().Sum(num1: 1, num2: 4)
+        print(sum)
     }
 }
 
